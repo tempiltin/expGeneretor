9851de43179ded651a85cc2fe7e7b83981e658ac
tempiltin <temurbekshukurov0707@gmail.com>
2022-07-19 22:57:19 +0500

    upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+let data = {
+  eage: "salom",
+  name: "Tempiltin"
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BookStore' });
+  res.status(200).send({
+    eage: "salom",
+    name: "Tempiltin"
+  })
+  console.log(window.location.pathname)
 });
 
 module.exports = router;
